a50d65fc0bfdcc0d193cfd01bd47d484c6592f12
Blueboltz <vincentmariscotescleofe@gmail.com>
2022-10-17 10:18:34 +0800

    First commit
    Added  logic to the stacks DS

--- myStackDS.cpp
+++ myStackDS.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
-#include <cstring>
+#include<iostream>
+#include<stack> // This library will let us use the functions like push and pop
+#include<string>
 
 using namespace std;
 
+// This function will check if curly braces are pairs or not
+bool areCurlyBracesPair(char opening,char closing) {
+	return (opening == '{' && closing == '}') ? true : false;
+}
+
+// This will check the if the curly braces are balanced
+bool areCurlyBraceBalanced(string exp) {
+	stack<char>  S;
+	for(int i =0;i<exp.length();i++)
+	{
+		if(exp[i] == '{')
+			S.push(exp[i]);
+		else if(exp[i] == '}')
+		{
+			if(S.empty() || !areCurlyBracesPair(S.top(),exp[i]))
+				return false;
+			else
+				S.pop();
+		}
+	}
+	return S.empty() ? true:false;
+}
+
 int main() {
-    
-    return 0;
+	string expression;
+	cout<<"Enter an expression:  "; // input expression from STDIN/Console
+	cin>>expression;
+	if(areCurlyBraceBalanced(expression))
+		cout<<"Balanced\n";
+	else
+		cout<<"Not Balanced\n";
 }
